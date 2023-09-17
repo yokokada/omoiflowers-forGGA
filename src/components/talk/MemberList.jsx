3559c40eb9c199faefc15a8b8548cadb32ea8b61
yokokada <yokoumasaka@gmail.com>
2023-09-17 13:11:07 +0900

    チャット画面CSS整え

--- src/components/talk/MemberList.jsx
+++ src/components/talk/MemberList.jsx
@@ -1,12 +1,14 @@
 import React, { useState, useEffect } from 'react';
 import { db, auth } from '../../pages/Firebase';  // FirestoreとAuthのインスタンスをインポート
-import { collection, getDocs, query, where, doc, getDoc } from "firebase/firestore";  // Firestoreの関数をインポート
+import { collection, getDocs } from "firebase/firestore";  // Firestoreの関数をインポート
 import { useNavigate } from 'react-router-dom';
+import "./Memberlist.css"
+import {Avatar} from "@nextui-org/react";
 
 const Memberlist = () => {
   const [members, setMembers] = useState([]);
   const  navigate  = useNavigate();
-
+  
   const handleChatWithMember = (memberId) => {
     navigate (`/chat/${memberId}`);
   };
@@ -16,44 +18,64 @@ const Memberlist = () => {
       const currentUser = auth.currentUser;  // 現在ログインしているユーザーを取得
       if (!currentUser) return;  // ユーザーがログインしていない場合、処理を終了
       
-      // ログインしているユーザーのtails IDを取得
-      const userDocRef = doc(db, 'users', currentUser.uid);
-      const userDocSnapshot = await getDoc(userDocRef);
-      const userTailId = userDocSnapshot.data().tail;
-      console.log(userTailId);
+      // // ログインしているユーザーのtails IDを取得
+      // const userDocRef = doc(db, 'users', currentUser.uid);
+      // const userDocSnapshot = await getDoc(userDocRef);
+      // const userTailId = userDocSnapshot.data().tail;
+      // console.log(userTailId);
 
-      // 取得したtailIdに属するメンバーを取得
-      const memberQuery = query(
-        collection(db, 'users'),
-        where('tail', '==', userTailId)
-      );
-      const memberSnapshot = await getDocs(memberQuery);
-      const memberData = memberSnapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
-      setMembers(memberData);
-    };
+    //   // 取得したtailIdに属するメンバーを取得
+    //   const memberQuery = query(
+    //     collection(db, 'users'),
+    //     where('tail', '==', userTailId)
+    //   );
+    //   const memberSnapshot = await getDocs(memberQuery);
+    //   const memberData = memberSnapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
+    //   setMembers(memberData);
+    // };
+    // すべてのユーザーを取得する新しいクエリ
+    const memberQuery = collection(db, 'users');
+    const memberSnapshot = await getDocs(memberQuery);
+    let memberData = memberSnapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
+    // 名前でソート
+    memberData.sort((a, b) => a.displayName.localeCompare(b.displayName, 'ja'));
+    // 現在のユーザーをリストの先頭に移動
+    const currentUserIndex = memberData.findIndex(member => member.id === currentUser.uid);
+    if (currentUserIndex > 0) {
+      const currentUserData = memberData.splice(currentUserIndex, 1)[0];
+      memberData.unshift(currentUserData);
+    }
+
+    setMembers(memberData);
+  };
     
     fetchMembers();
   }, []);
 
   return (
     <div>
-      <h1 style={{fontSize:'20px' ,color:'#1B3672' ,marginBottom:'10px'}} >メンバーリスト</h1>
+      <h1 className="member-header">メンバーリスト</h1>
       <ul>
       {members.map(member => (
         <li key={member.id}>
-          <div className="inline-flex items-center justify-center gap-4 rounded-small bg-white px-6 py-2 m-4 shadow-lg"
+          <div className="member-item"
           onClick={() => handleChatWithMember(member.id)} >
-            <span className="flex relative justify-center items-center box-border overflow-hidden align-middle z-20 w-20 h-20 text-base bg-default text-default-foreground rounded-full">
-              <img 
-                src={member.avatar || "デフォルトの画像のURL"} 
-                alt={`${member.displayName}のアバター`} 
-                className="flex object-cover w-14 h-14 transition-opacity opacity-0 rounded-full shadow-lg"
-                onLoad={(e) => e.target.style.opacity = 1}
-              />
+            <span className="member-avatar">
+            { member.avatar ? (
+            <img 
+              src={member.avatar} 
+              alt={`${member.displayName}のアバター`} 
+              className="member-image"
+              onLoad={(e) => e.target.style.opacity = 1}
+            />
+            ) : (
+            <Avatar size="140px" color="#DDDDDD" />
+            )
+            }
             </span>
-            <div className="inline-flex flex-col items-start">
-              <span className="text-large text-inherit">{member.displayName}</span>
-              <span className="text-base text-foreground-400">{member.email}</span>
+            <div className="member-detail">
+              <span className="member-name">{member.displayName}</span>
+              {/* <span className="member-email">{member.email}</span> */}
             </div>
           </div>
         </li>
--- src/pages/Talks.jsx
+++ src/pages/Talks.jsx
@@ -12,12 +12,12 @@ const Talks = () => {
   return (
     <div>
       <TopNavbar/>
-      <div className='menu' style={{ marginTop: "100px", marginBottom: "30px", marginLeft: "40px"}}>
+      <div className='menu' style={{ marginTop: "80px", marginBottom: "30px", marginLeft: "40px"}}>
         <div style={{ display:"flex" }}>
-        <h1  style={{fontSize:'20px' ,color:'#1B3672'}} onClick={() => navigate("/all-post")}>全員に発信　</h1><Send fontSize={20} strokeWidth={1} color='#1B3672'/>
+        <h1  style={{fontSize:'16px' ,color:'#1B3672',fontWeight:'bold'}} onClick={() => navigate("/all-post")}>全員に一括で発信　</h1><Send fontSize={16} strokeWidth={1.5} color='#1B3672'/>
         </div>
         <div style={{ display:"flex",marginTop:'20px' }}>
-          <h1 style={{fontSize:'20px',color:'#1B3672' }}>個別に選んで発信　</h1><ArrowDown  fontSize={18} strokeWidth={1} color='#1B3672'/>
+          <h1 style={{fontSize:'16px',color:'#1B3672' ,fontWeight:'bold'}}>個別に選んで発信　</h1><ArrowDown  fontSize={14} strokeWidth={2} color='#1B3672'/>
         </div>
        </div>
        <MemberList/>
